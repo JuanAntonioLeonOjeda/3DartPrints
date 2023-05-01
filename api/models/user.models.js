a1474ce55f4f410e6302a28ffef2e2ab70178989
JuanAntonioLeonOjeda <jalojr@hotmail.com>
2023-05-01 14:07:59 +0100

    test

--- api/models/user.models.js
+++ api/models/user.models.js
@@ -7,52 +7,52 @@ const User = sequelize.define(
         name: {
             type: DataTypes.STRING,
             allowNull: false,
-        },
-        surname: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        username: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        email: {
-            type: DataTypes.STRING,
-            validate: {
-                is: {
-                    args: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                    msg: "Error: Wrong email format."
-                }
-            }
-        },
-        birth: {
-            type: DataTypes.DATE,
-        },
-        phone: {
-            type: DataTypes.INTEGER,
-        },
-        address: {
-            type: DataTypes.STRING,
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        verified: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-        },
-        role: {
-            type: DataTypes.ENUM('admin', 'user'),
-            allowNull: false,
-        },
-        designer: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-        },
-        printer: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
+        // },
+        // surname: {
+        //     type: DataTypes.STRING,
+        //     allowNull: false,
+        // },
+        // username: {
+        //     type: DataTypes.STRING,
+        //     allowNull: false,
+        // },
+        // email: {
+        //     type: DataTypes.STRING,
+        //     validate: {
+        //         is: {
+        //             args: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        //             msg: "Error: Wrong email format."
+        //         }
+        //     }
+        // },
+        // birth: {
+        //     type: DataTypes.DATE,
+        // },
+        // phone: {
+        //     type: DataTypes.INTEGER,
+        // },
+        // address: {
+        //     type: DataTypes.STRING,
+        // },
+        // password: {
+        //     type: DataTypes.STRING,
+        //     allowNull: false,
+        // },
+        // verified: {
+        //     type: DataTypes.BOOLEAN,
+        //     allowNull: false,
+        // },
+        // role: {
+        //     type: DataTypes.ENUM('admin', 'user'),
+        //     allowNull: false,
+        // },
+        // designer: {
+        //     type: DataTypes.BOOLEAN,
+        //     allowNull: false,
+        // },
+        // printer: {
+        //     type: DataTypes.BOOLEAN,
+        //     allowNull: false,
 
 /*         createdAt: {
             type: DataTypes.STRING,
